38a03e249ba04597e4e7317d34943cc59e99cfec
Siddhant Sapru <siddhantsapru@Siddhants-MacBook-Pro.local>
2024-06-22 01:45:34 -0400

    dev1.1 to incorporate new changes for a seperate Grocery entity

---
+++ quickeats/src/main/java/com/project/quickeats/QuickeatsApplication.java
@@ -0,0 +1,13 @@
+package com.project.quickeats;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickeatsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuickeatsApplication.class, args);
+	}
+
+}
---
+++ quickeats/src/test/java/com/project/quickeats/QuickeatsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.quickeats;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuickeatsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
